0b86f820adba07ab745db589de2ea4cdd68ae48a
James Healy <jimmy@deefa.com>
2012-04-19 14:34:56 +1000

    there's not really any need for a class here, it's just a small glue script

--- testify.rb
+++ testify.rb
@@ -7,32 +7,27 @@ require 'librato_updater'
 
 CHROME_PATH = '"google-chrome"'
 
-class Testify
-
-  def initialize
-    librato_updater = LibratoUpdater.new
-
-    Configuration.instance.config['pages'].collect {|name, url|
-      chrome = ChromeRemoteDebugger.new url
-      {
-        requests:             chrome.document.request_count,
-        onload_event:         chrome.document.onload_event,
-        dom_content_event:    chrome.document.dom_content_event,
-        script_payload:       chrome.document.size("Script"),
-        image_payload:        chrome.document.size("Image"),
-        stylesheet_payload:   chrome.document.size("Stylesheet"),
-        script_count:         chrome.document.request_count_by_resource("Script"),
-        image_count:          chrome.document.request_count_by_resource("Image"),
-        stylesheet_count:     chrome.document.request_count_by_resource("Stylesheet")
-      }.each {|key, value| librato_updater.queue "#{name}.#{key}", value }
-    }
-    librato_updater.submit()
-  end
-
-end
-
 chrome_pid = Process.spawn("#{CHROME_PATH} --user-data-dir=/tmp/temp-profile -remote-debugging-port=9222", :pgroup => true)
 sleep 2
-testify = Testify.new
 
-Process.kill('-TERM', Process.getpgid(chrome_pid))
+librato_updater = LibratoUpdater.new
+
+Configuration.instance.config['pages'].collect {|name, url|
+  chrome = ChromeRemoteDebugger.new url
+  {
+    requests:             chrome.document.request_count,
+    onload_event:         chrome.document.onload_event,
+    dom_content_event:    chrome.document.dom_content_event,
+    script_payload:       chrome.document.size("Script"),
+    image_payload:        chrome.document.size("Image"),
+    stylesheet_payload:   chrome.document.size("Stylesheet"),
+    script_count:         chrome.document.request_count_by_resource("Script"),
+    image_count:          chrome.document.request_count_by_resource("Image"),
+    stylesheet_count:     chrome.document.request_count_by_resource("Stylesheet")
+  }.each {|key, value| librato_updater.queue "#{name}.#{key}", value }
+}
+#librato_updater.submit()
+
+puts Process.kill('-TERM', Process.getpgid(chrome_pid))
+
+exit 0
