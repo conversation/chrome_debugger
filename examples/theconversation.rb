325f6313c647945fb7b749cd1dfc987d380c2774
James Healy <jimmy@deefa.com>
2012-05-03 00:49:08 +1000

    move testify.rb into a simple exampel script

--- examples/theconversation.rb
+++ examples/theconversation.rb
@@ -1,22 +1,9 @@
-$LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
-
 require 'chrome_debugger'
-require 'librato/metrics'
 
-# Config
-
-LIBRATO_PREFIX = "tc.frontend"
-LIBRATO_USER   = ""
-LIBRATO_KEY    = ""
 PAGES          = {
   homepage: 'https://theconversation.edu.au/',
   articlepage: 'https://theconversation.edu.au/bike-lanes-economic-benefits-go-beyond-jobs-6081/'
 }
-
-# ACTION
-
-Librato::Metrics.authenticate(LIBRATO_USER, LIBRATO_KEY)
-librato_queue = Librato::Metrics::Queue.new
 
 PAGES.each do |name, url|
   ChromeDebugger::Client.open do |chrome|
@@ -39,11 +26,7 @@ PAGES.each do |name, url|
       image_count:          document.request_count_by_resource("Image"),
       stylesheet_count:     document.request_count_by_resource("Stylesheet")
     }.each {|key, value|
-      puts "#{LIBRATO_PREFIX}.#{name}.#{key}: #{value}"
-    }.each { |key, value|
-      librato_queue.add("#{LIBRATO_PREFIX}.#{name}.#{key}" => value)
+      puts "#{name}.#{key}: #{value}"
     }
   end
 end
-
-librato_queue.submit
